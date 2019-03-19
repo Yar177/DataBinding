cac772b41daa88af9659b296ac090d3e31c4dfac
Yar <yarsher.dev@gmail.com>
2019-03-19 15:52:35 -0400

    ActicityMainBinding

--- app/src/main/java/com/yarsher/at/databinding/MainActivity.java
+++ app/src/main/java/com/yarsher/at/databinding/MainActivity.java
@@ -1,13 +1,24 @@
 package com.yarsher.at.databinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.yarsher.at.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        ContactInfo contactInfo = new ContactInfo("Yar", "Sher");
+
+        activityMainBinding.setContact(contactInfo);
+
+
     }
 }
