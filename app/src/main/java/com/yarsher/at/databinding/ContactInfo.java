296021691d010547fbe0f01f50433e9b9feeeaaa
Yar <yarsher.dev@gmail.com>
2019-03-19 15:41:49 -0400

    ContactData.java

---
+++ app/src/main/java/com/yarsher/at/databinding/ContactInfo.java
@@ -0,0 +1,14 @@
+package com.yarsher.at.databinding;
+
+public class ContactInfo {
+    public final String firstName;
+    public final String lastName;
+
+    public ContactInfo(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+}
