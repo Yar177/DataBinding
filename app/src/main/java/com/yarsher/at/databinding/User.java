f087e8dc6240b372634b62d097f388e6895df9ca
Yar <yarsher.dev@gmail.com>
2019-03-20 11:13:43 -0400

    User init

---
+++ app/src/main/java/com/yarsher/at/databinding/User.java
@@ -0,0 +1,35 @@
+package com.yarsher.at.databinding;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class User extends BaseObservable {
+
+    private String firstName;
+    private String lastName;
+
+    @Bindable
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Bindable
+    public String getLastName() {
+        return lastName;
+    }
+
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
+    }
+
+
+
+}
