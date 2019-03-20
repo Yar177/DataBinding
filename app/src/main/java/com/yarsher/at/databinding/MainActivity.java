e545fb9e9045c688eb71bc5cd1bd61b298e5e250
Yar <yarsher.dev@gmail.com>
2019-03-20 09:51:31 -0400

    clickHandler

--- app/src/main/java/com/yarsher/at/databinding/MainActivity.java
+++ app/src/main/java/com/yarsher/at/databinding/MainActivity.java
@@ -1,8 +1,11 @@
 package com.yarsher.at.databinding;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.yarsher.at.databinding.databinding.ActivityMainBinding;
 
@@ -16,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         ContactInfo contactInfo = new ContactInfo("Yar", "Sher");
         activityMainBinding.setContact(contactInfo);
+        setSupportActionBar(activityMainBinding.myToolbar);
+
+    }
+
+
+    public class clickHandler{
+
+        private Context context;
+
+        public clickHandler(Context context) {
+            this.context = context;
+        }
+
+        public void onButtonClick(View view){
+            Toast.makeText(context, "Button Click Event", Toast.LENGTH_LONG).show();
+        }
+
+
+
 
 
     }
